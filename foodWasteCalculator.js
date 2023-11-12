b545ec836c18b333f267e366a7ede14854e4c51c
Dylan Rylee A. Dizon <109629195+dylanrylee@users.noreply.github.com>
2023-11-11 23:18:18 -0700

    GG WP FOR ALL

---
+++ foodWasteCalculator.js
@@ -0,0 +1,12 @@
+let numOfPrice = 0.00;
+let quantityOfItems = 0;
+let numOfWasted = 0;
+
+
+class foodWasteCalculator {
+    constructor(numOfPrice, quantityOfItems) {
+        this.numOfPrice = numOfPrice; 
+        this.quantityOfItems = quantityOfItems;
+        this.numOfWasted = numOfWasted;
+    }
+}
